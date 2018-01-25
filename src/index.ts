ee5e71f29213c156df3ddae0114a446c7cc87b10
Przemysław Afeltowicz <pafeltowicz@cyfrowypolsat.pl>
2018-01-25 22:58:13 +0100

    Zmiany

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export default async function asyncMap(lists: Array<any>, callback: Function) {
+export async function asyncMap(lists: Array<any>, callback: Function) {
     const results = [];
     for await (let item of lists) {
         const itemResult = await callback(item);
@@ -6,3 +6,8 @@ export default async function asyncMap(lists: Array<any>, callback: Function) {
     }
     return results;
 }
+
+export const test = {
+    test: {},
+    async: asyncMap
+};
